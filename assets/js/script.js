48065a8ff91907d422b42cae2ace5d784e80a2c8
Daniel <dandanielofc@gmail.com>
2022-03-20 12:33:44 -0300

    bug fix animação mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-ScrollReveal().reveal('.inicio', { delay: 200, distance: '50px' });
-ScrollReveal().reveal('.header-hero-img', { delay: 300, distance: '50px' });
-ScrollReveal().reveal('.btn-saibamais', { delay: 500, scale: 0.85 });
-ScrollReveal().reveal('.Header-Produtos', { delay: 500, scale: 0.85 });
-ScrollReveal().reveal('.produtos', { delay: 1000, distance: '50px' });
-ScrollReveal().reveal('.selo', { delay: 1500, scale: 0.85 });
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.inicio', { delay: 200, distance: '50px' });
+sr.reveal('.header-hero-img', { delay: 300, distance: '50px' });
+sr.reveal('.btn-saibamais', { delay: 500, scale: 0.85 });
+sr.reveal('.Header-Produtos', { delay: 500, scale: 0.85 });
+sr.reveal('.produtos', { delay: 1000, distance: '50px' });
+sr.reveal('.selo', { delay: 1500, scale: 0.85 });
+sr.reveal('.perguntas', { delay: 1000, distance: '50px' });
+
 
 const accordion = document.getElementsByClassName('container-acc');
 for (i=0; i<accordion.length; i++) {
